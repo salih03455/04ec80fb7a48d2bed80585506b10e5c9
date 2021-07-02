8e2c18aa6732a044b3e76d485d45d50dc3ff21bc
Salih <salih@Salih-MacBook-Pro.local>
2021-07-02 11:39:22 +0300

    package.json guncellendi

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,61 @@
+<script>
+import HotelDatePicker from 'vue-hotel-datepicker2'
+import 'vue-hotel-datepicker2/dist/vueHotelDatepicker2.css'
+
+export default {
+  data() {
+    return {
+      trTR: {
+        "check-in": "Giriş",
+        "check-out": "Çıkış",
+        "day-names": ["Pt", "Sa", "Ça", "Pe", "Cu", "Ct", "Pa"],
+        "month-names": [
+          "Ocak",
+          "Şubat",
+          "Mart",
+          "Nisan",
+          "Mayıs",
+          "Haziran",
+          "Temmuz",
+          "Ağustos",
+          "Eylül",
+          "Ekim",
+          "Kasım",
+          "Aralık",
+        ],
+        night: "gece",
+        nights: "gece",
+        tooltip: {
+          halfDayCheckIn: "Rezervasyon müsait",
+          halfDayCheckOut: "Rezervasyon müsait",
+          saturdayToSaturday: "Sadece cumartesiden<br/> cumartesiye",
+          sundayToSunday: "Sadece pazardan<br/> pazara",
+          minimumRequiredPeriod: "%{minNightInPeriod} %{night} en az",
+        },
+        week: "hafta",
+        weeks: "hafta",
+      }
+    }
+  },
+  components: {
+    HotelDatePicker
+  }
+}
+</script>
+
 <template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    
+    <HotelDatePicker
+      format="DD/MM/YYYY"
+      :showYear="true"
+      :i18n="trTR">
+    </HotelDatePicker>
+    
     <router-view/>
   </div>
 </template>
